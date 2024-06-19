2569fe4d0cf70df9b80b44a1166eb35a928a3add
muhanurozulgyuyo <158283090+muhanurozulgyuyo@users.noreply.github.com>
2024-06-19 20:25:40 +0900

    Time: 44 ms (19.80%), Space: 16.7 MB (25.79%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -1,27 +1,57 @@
-
+import string
 
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        # 1. 키의 첫 번째 등장 순서 추출
-        seen = set()
-        substitution_table = {}
-        alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        index = 0
         
-        for char in key:
-            if char not in seen and char != ' ':
-                seen.add(char)
-                substitution_table[char] = alphabet[index]
-                index += 1
+        converter = {}
+        alphabet = string.ascii_lowercase
+        answer = []
         
-        # 2. 메시지 복호화
-        decoded_message = []
+        key = key.replace(' ', '')
         
-        for char in message:
-            if char == ' ':
-                decoded_message.append(' ')
+        key_cnt = 0
+        alp_cnt = 0
+        
+        while alp_cnt < len(alphabet):
+            
+            if key[key_cnt] not in converter.keys():
+                converter[key[key_cnt]] = alphabet[alp_cnt]
+                key_cnt += 1
+                alp_cnt += 1
             else:
-                decoded_message.append(substitution_table[char])
+                key_cnt += 1
+            
+        for s in message:
+            
+            if s == ' ':
+                answer.append(' ')
+            else:
+                answer.append(converter[s])
+                
+        return ''.join(answer)
+
+# class Solution:
+#     def decodeMessage(self, key: str, message: str) -> str:
+#         # 1. 키의 첫 번째 등장 순서 추출
+#         seen = set()
+#         substitution_table = {}
+#         alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#         index = 0
         
-        return ''.join(decoded_message)
+#         for char in key:
+#             if char not in seen and char != ' ':
+#                 seen.add(char)
+#                 substitution_table[char] = alphabet[index]
+#                 index += 1
+        
+#         # 2. 메시지 복호화
+#         decoded_message = []
+        
+#         for char in message:
+#             if char == ' ':
+#                 decoded_message.append(' ')
+#             else:
+#                 decoded_message.append(substitution_table[char])
+        
+#         return ''.join(decoded_message)
            
