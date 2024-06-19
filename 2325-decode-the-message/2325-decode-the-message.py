a76d7dc2726045243f88fb67d9af4be686fd495a
muhanurozulgyuyo <158283090+muhanurozulgyuyo@users.noreply.github.com>
2024-06-19 20:23:54 +0900

    Time: 44 ms (19.80%), Space: 16.6 MB (70.64%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -1,32 +1,57 @@
-import string
+# import string
+
+# class Solution:
+#     def decodeMessage(self, key: str, message: str) -> str:
+        
+#         converter = {}
+#         alphabet = string.ascii_lowercase
+#         answer = []
+        
+#         key = key.replace(' ', '')
+        
+#         key_cnt = 0
+#         alp_cnt = 0
+        
+#         while alp_cnt < len(alphabet):
+            
+#             if key[key_cnt] not in converter.keys():
+#                 converter[key[key_cnt]] = alphabet[alp_cnt]
+#                 key_cnt += 1
+#                 alp_cnt += 1
+#             else:
+#                 key_cnt += 1
+            
+#         for s in message:
+            
+#             if s == ' ':
+#                 answer.append(' ')
+#             else:
+#                 answer.append(converter[s])
+                
+#         return ''.join(answer)
 
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
+        # 1. 키의 첫 번째 등장 순서 추출
+        seen = set()
+        substitution_table = {}
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        index = 0
         
-        converter = {}
-        alphabet = string.ascii_lowercase
-        answer = []
+        for char in key:
+            if char not in seen and char != ' ':
+                seen.add(char)
+                substitution_table[char] = alphabet[index]
+                index += 1
         
-        key = key.replace(' ', '')
+        # 2. 메시지 복호화
+        decoded_message = []
         
-        key_cnt = 0
-        alp_cnt = 0
-        
-        while alp_cnt < len(alphabet):
-            
-            if key[key_cnt] not in converter.keys():
-                converter[key[key_cnt]] = alphabet[alp_cnt]
-                key_cnt += 1
-                alp_cnt += 1
+        for char in message:
+            if char == ' ':
+                decoded_message.append(' ')
             else:
-                key_cnt += 1
-            
-        for s in message:
-            
-            if s == ' ':
-                answer.append(' ')
-            else:
-                answer.append(converter[s])
-                
-        return ''.join(answer)
-            
+                decoded_message.append(substitution_table[char])
+        
+        return ''.join(decoded_message)
+           
